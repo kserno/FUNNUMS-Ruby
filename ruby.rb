6fb6486a9cf56695c31c068236463144ee6f8ccd
kserno <filip.sollar@gmail.com>
2015-06-12 12:07:03 +0200

    Working! but getting TLE

--- ruby.rb
+++ ruby.rb
@@ -1,34 +1,50 @@
-#abcd 1234
-#abdc 1243
-#acbd 1324
-#acdb 1342
-#adbc 1423
-#adcb 1432
-#bacd 2134
-#badc 2143
-#bcad 2314
-#bcda 2341
-#bdac 2413
-#bdca 2431
-#cabd 3124
-#cadb 3142
-#cbad 3214
-#cbda 3241
-#cdab 3412
-#cdba 3421
-#dabc 4123
-#dacb 4132
-#dbac 4213
-#dbca 4231
-#dcab 4312
-#dcba 4321
+def fact(n)
+x=1
+for i in 1..n
+x=x*i
+end
+return x
+end
 
-#6,2,1
-#24/3! = 4
-#24/2! = 12/4 = 3
-#24/1! = 24/4=4/3=2
-#24/0! = 24/4=4/3=2/2=1
-#23/3! = 4
-#23/2! = 12/4
-#23/1! = 12/4 = 3/3 = 1  
+def pos(n)
+arr=n.to_s.chars
+tarr=arr.sort
+pos = 0
+for i in 0..arr.length-2
+f= find_index(tarr, arr[i])
+d = fact(arr.length-i-1)*(f-1)
+tarr.delete(arr[i])
+pos = pos + d	
+end
+return pos
+end
 
+def find_number(position,n)
+arr=n.to_s.chars
+arr=arr.sort
+s=""
+x=arr.length
+for i in 0..x-2
+zv = position % fact(x-i-1)
+po= position/fact(x-i-1)
+s = s+arr[po]
+arr.delete(arr[po])
+position=zv
+end
+s=s+arr[0]
+return s
+end
+
+def find_index(arr,index)
+for i in 0..arr.length-1
+if (arr[i]==index) then return i+1
+end
+end
+end
+t= gets
+for i in 1..t.to_i
+n = gets
+x = n.strip.split(/\s+/)
+g = pos(x[0])
+puts find_number(g+x[1].to_i, x[0])
+end
